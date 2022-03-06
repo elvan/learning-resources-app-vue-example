975dd100e4fd7220de6597c1344c69535ecf5d34
Elvan H <elvanhd@gmail.com>
2022-03-06 11:04:02 +0700

    creating example data

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      storedResources: [
+        {
+          id: '1',
+          title: 'Vue Documentation',
+          description: 'The official Vue.js documentation.',
+          url: 'https://vuejs.org/',
+        },
+        {
+          id: '2',
+          title: 'Vue Router',
+          description: 'A modern router for Vue.js.',
+          url: 'https://router.vuejs.org/',
+        },
+        {
+          id: '3',
+          title: 'Vuex',
+          description: 'A state management pattern + library for Vue.js.',
+          url: 'https://vuex.vuejs.org/',
+        },
+        {
+          id: '4',
+          title: 'Vue CLI',
+          description: 'A Vue.js development toolkit.',
+          url: 'https://cli.vuejs.org/',
+        },
+      ],
+    };
+  },
+
+  methods: {},
+};
+</script>
