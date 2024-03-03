b1c044188bf5814d3d021d4ca811d209e7011750
Elvan Hidayat <elvanhd@gmail.com>
2024-03-03 20:12:04 +0700

    Refactor resource listing and add global styles
    - Replaced direct listing in App.vue with StoredResources component
    - StoredResources component now handles the rendering of resources
    - Added global Roboto font and base styles to App.vue for consistency
    - Updated LearningResource.vue with scoped styling for better layout
    - Introduced Scoped CSS in LearningResource for improved appearance
    - Added new StoredResources component to manage resource list
    - Global styles include box-sizing, font-family, and body margin reset

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -18,3 +18,35 @@ export default {
   props: ['title', 'description', 'link'],
 };
 </script>
+
+<style scoped>
+li {
+  max-width: 40rem;
+  margin: auto;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  margin: 0.5rem 0;
+  font-size: 1.25rem;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  color: #ce5c00;
+  text-decoration: none;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  props: ['resources'],
+  components: {
+    LearningResource,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  max-width: 40rem;
+  margin: 0;
+  margin: auto;
+  padding: 0;
+  list-style: none;
+}
+</style>
