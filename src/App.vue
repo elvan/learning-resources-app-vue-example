76562335ed368d5f188a944f25d092749ca44b19
Elvan Hidayat <elvanhd@gmail.com>
2024-03-03 20:37:28 +0700

    Refactor resources management and add resource form
    - Centralized resources data and logic in TheResources component
    - Introduced TheResources component to toggle between stored resources
      and adding new resources
    - Implemented dynamic component switching in TheResources for UI tabs
    - Added AddResource component as a placeholder for resource creation
    - Changed StoredResources to use dependency injection for resources
    - Removed resources data array from App.vue to TheResources component
    - Provided resources data from TheResources to children via provide
    - Added computed properties for dynamic tab button styling in TheResources

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
 <template>
   <the-header title="RememberMe"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <the-resources></the-resources>
 </template>
-å
 
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
-import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
-    StoredResources,
     TheHeader,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation.',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.com',
-        },
-      ],
-    };
+    TheResources,
   },
 };
 </script>
